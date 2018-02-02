afad445c0b6570bf52c6190bef3688c8feeacd57
Trevor Murphy <murphtrevor@gmail.com>
2018-02-01 21:53:20 -0600

    Add new project file.

--- a1/collatz.c
+++ a1/collatz.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
 void collatz(int n) {
+  /**
+  * Collatz Conjecture:
+  * n = n/2 if n is even
+  * n = 3 * n + 1 if n is odd
+  */
   if (n == 1) {
     printf("%d\n", n);
   } else {
@@ -19,9 +22,14 @@ void collatz(int n) {
 
 int main(int argc, char const *argv[]) {
 
+  if (argc != 2) {
+    puts("Please provide one argument!");
+    return 1;
+  }
+
   int startNum = atoi(argv[1]);
 
-  if (argc != 2 || startNum < 0) {
+  if (startNum < 0) {
     printf("Please only supply one positive number as an argument!\n");
     return 1;
   }
@@ -30,7 +38,7 @@ int main(int argc, char const *argv[]) {
   pid = fork();
 
   if (pid < 0) {
-    fprintf(stderr, "ForkFailed\n");
+    fprintf(stderr, "Fork Failed!\n");
     return 1;
   } else if (pid == 0) {
     printf("Child Process Executing\n");
---
+++ a1/shell.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAX_LINE 80 // The maximum length command
+#define MAX_SIZE MAX_LINE / 2 + 1
+
+int main(int argc, char const *argv[]) {
+  char input[MAX_SIZE];
+  char * args[MAX_SIZE]; // command line arguments
+  char * token;
+
+  int shouldRun = 1; // flag to determine when to exit program
+  while (shouldRun) {
+    memset(args, 0, sizeof(args)); // Clear input before retrieving it again
+    printf("osh> ");
+    fflush(stdout);
+    fgets(input, sizeof(input), stdin);
+    input[strcspn(input, "\n")] = '\0'; // Strip newline from input since it is a pain
+    if (strcmp(input, "exit") == 0) {
+      shouldRun = 0;
+      continue;
+    }
+    token = strtok(input, " ");
+    int i = 0;
+    while (token != NULL) {
+      args[i] = strdup(token);
+      token = strtok(NULL, " ");
+      ++i;
+    }
+
+    pid_t pid;
+    pid = fork();
+    if (pid < 0) {
+      fprintf(stderr, "Fork Failed!\n");
+      return 1;
+    } else if (pid == 0) {
+      printf("CHILD\n");
+      execvp(args[0], args);
+    } else {
+      wait(NULL);
+    }
+  }
+  return 0;
+}
