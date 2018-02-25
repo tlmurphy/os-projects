66a61a70d046c5da543cae93382343a84b735a46
Trevor Murphy <murphtrevor@gmail.com>
2018-02-24 19:19:42 -0600

    Begin assignment 3

---
+++ a3/p536.c
@@ -0,0 +1,70 @@
+/**
+ * Exercise 5.36
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define MIN_PID 300
+#define MAX_PID 5000
+#define NUM_THREADS 100
+
+int counter = 0;
+int bit_map[MAX_PID];
+pthread_mutex_t mutex;
+
+int allocate_map(void) {
+  for (size_t i = MIN_PID; i < MAX_PID; i++) {
+    bit_map[i] = 0;
+  }
+  return 1;
+}
+
+int allocate_pid(void) {
+  for (size_t i = MIN_PID; i < MAX_PID; i++) {
+    if (bit_map[i] == 0) {
+      bit_map[i] = 1;
+      return i;
+    }
+  }
+  return -1;
+}
+
+void release_pid(int pid) {
+  bit_map[pid] = 0;
+}
+
+void *runner(void *param) {
+  pthread_mutex_lock(&mutex);
+  counter++;
+  int pid = allocate_pid();
+  int sleep_time = (rand() % 20) + 1;
+  printf("Sleeping Time: %d; Thread Id = %d; Counter Value = %d\n", sleep_time, pid, counter);
+  pthread_mutex_unlock(&mutex);
+  sleep(sleep_time); // Random number from 1 -> 20
+  release_pid(pid);
+  pthread_exit(0);
+}
+
+int main(int argc, char const *argv[]) {
+  srand(time(NULL));
+  pthread_t workers[NUM_THREADS];
+  pthread_attr_t attr;
+
+  pthread_attr_init(&attr);
+  pthread_mutex_init(&mutex, NULL);
+  allocate_map();
+  int i = 0;
+  while (i < NUM_THREADS) {
+    pthread_create(&workers[i], &attr, runner, NULL);
+    i++;
+  }
+  i = 0;
+  while (i < NUM_THREADS) {
+    pthread_join(workers[i], NULL);
+    i++;
+  }
+  return 0;
+}
---
+++ a3/pp3.c
@@ -0,0 +1,102 @@
+/**
+ * Programming Project 3
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 5
+
+typedef int buffer_item;
+buffer_item buffer[BUFFER_SIZE];
+pthread_mutex_t mutex;
+sem_t empty, full;
+int b_index; // Keep track of buffer index
+
+int insert_item(buffer_item item) {
+  if (b_index < BUFFER_SIZE) {
+    buffer[b_index] = item;
+    b_index++;
+    return 0;
+  } else {
+    return -1
+  }
+}
+
+int remove_item(buffer_item *item) {
+  if (b_index > 0) {
+    *item = buffer[b_index - 1];
+    b_index--;
+    return 0;
+  } else {
+    return -1;
+  }
+}
+
+void *producer(void *param) {
+  buffer_item item;
+  while (1) {
+    sleep((rand() % 10) + 1);
+    item = rand();
+    sem_wait(&empty);
+    pthread_mutex_lock(&mutex);
+    if (insert_item(item)) {
+      puts("Error on inserting item!");
+    } else {
+      printf("producer produced %d\n", item);
+    }
+    pthread_mutex_unlock(&mutex);
+    sem_post(&full);
+  }
+}
+
+void *consumer(void *param) {
+  buffer_item item;
+  while (1) {
+    sleep((rand() % 10) + 1);
+    sem_wait(&full);
+    pthread_mutex_lock(&mutex);
+    if (remove_item(&item)) {
+      puts("Error on removing item!");
+    } else {
+      printf("consumer consumed %d\n", item);
+    }
+    pthread_mutex_unlock(&mutex);
+    sem_post(&empty);
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  if (argc != 4) {
+    puts("Please supply 3 arguments!");
+    return -1;
+  }
+
+  long sleep_time = strtol(argv[1], NULL, 0);
+  long n_producers = strtol(argv[2], NULL, 0);
+  long n_consumers = strtol(argv[3], NULL, 0);
+
+  srand(time(NULL));
+  sem_init(&empty, 0, BUFFER_SIZE); // Empty will count num of empty buffers
+  sem_init(&full, 0, 0); // Full will count num of full buffers
+  pthread_mutex_init(&mutex, NULL);
+  b_index = 0;
+
+  pthread_t prod[n_producers];
+  pthread_t cons[n_consumers];
+  pthread_attr_t attr;
+  pthread_attr_init(&attr);
+
+  for (size_t i = 0; i < n_producers; i++) {
+    pthread_create(&prod[i], &attr, producer, NULL);
+  }
+  for (size_t i = 0; i < n_consumers; i++) {
+    pthread_create(&cons[i], &attr, consumer, NULL);
+  }
+  sleep(sleep_time);
+  return 0;
+}
